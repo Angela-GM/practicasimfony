3459d968fee0665b9535594cabe27bd3621921e5
Angela-GM <angelagm0112@gmail.com>
2023-08-25 16:15:53 +0200

    agregar en el controlador de proveedores el metodo para mostar todos y por id

--- src/Controller/ProveedorController.php
+++ src/Controller/ProveedorController.php
@@ -2,17 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Proveedor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class ProveedorController extends AbstractController
 {
-    #[Route('/proveedor', name: 'app_proveedor')]
-    public function index(): Response
+    private $em;
+    public function __construct(EntityManagerInterface $em)
     {
+        $this->em = $em;
+    }
+
+    
+    // #[Route('/proveedor', name: 'app_proveedor')]
+    // public function index(Proveedor $proveedores): Response
+    // {
+    //     return $this->render('proveedor/index.html.twig', [
+    //         'controller_name' => 'ProveedorController',
+    //         'proveedores' => $proveedores->findAll()
+    //     ]);
+    // }
+
+    // #[Route('/proveedor/{id}', name: 'proveedor_id')]
+    // public function allProveedor(Proveedor $proveedor): Response
+    // {
+    //     dump($proveedor);
+    //     return $this->render('proveedor/index.html.twig', [
+    //         'controller_name' => 'ProveedorController',
+    //         'proveedor' => $proveedor
+                
+    //     ]);
+    // }
+
+    
+    // #[Route('/proveedor/{id}', name: 'proveedor_id')]
+    // public function findForId($id): Response
+    // {
+    //     $proveedor = $this->em->getRepository(Proveedor::class)->find($id);
+       
+    //     return $this->render('proveedor/index.html.twig', [
+    //         'proveedor' => $proveedor
+                
+    //     ]);
+    // }
+
+    #[Route('/proveedor', name: 'all_proveedor')]
+    public function allProveedor(): Response
+    {
+        $proveedores = $this->em->getRepository(Proveedor::class)->findAll();
+       
         return $this->render('proveedor/index.html.twig', [
-            'controller_name' => 'ProveedorController',
+            'proveedores' => $proveedores
+                
         ]);
     }
+
+    
 }
