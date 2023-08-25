ef5a936d4d0c80665ade572afb3689d48ad9f9ed
Angela-GM <angelagm0112@gmail.com>
2023-08-25 14:05:21 +0200

    crear la entidad proveedor

---
+++ src/Entity/Proveedor.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProveedorRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProveedorRepository::class)]
+class Proveedor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $correo = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $telefono = null;
+
+    #[ORM\Column]
+    private ?bool $activo = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $fecha_creacion = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $fecha_actualizacion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getCorreo(): ?string
+    {
+        return $this->correo;
+    }
+
+    public function setCorreo(string $correo): static
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(string $telefono): static
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function isActivo(): ?bool
+    {
+        return $this->activo;
+    }
+
+    public function setActivo(bool $activo): static
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
+
+    public function getFechaCreacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_creacion;
+    }
+
+    public function setFechaCreacion(\DateTimeInterface $fecha_creacion): static
+    {
+        $this->fecha_creacion = $fecha_creacion;
+
+        return $this;
+    }
+
+    public function getFechaActualizacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_actualizacion;
+    }
+
+    public function setFechaActualizacion(\DateTimeInterface $fecha_actualizacion): static
+    {
+        $this->fecha_actualizacion = $fecha_actualizacion;
+
+        return $this;
+    }
+}
