f3966ff7fed5532ced5d5b1472a0d9ed49146c98
Angela-GM <angelagm0112@gmail.com>
2023-08-26 17:11:17 +0200

    crear los botonoes y funcionalidad de los formularios para volver al listado de proveedores

--- src/Form/ProveedorType.php
+++ src/Form/ProveedorType.php
@@ -9,11 +9,22 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Tipo;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+
+
 
 
 
 class ProveedorType extends AbstractType
 {
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -26,6 +37,12 @@ class ProveedorType extends AbstractType
                 'choice_label' => 'tipo', 
             ])   
             ->add('submit', SubmitType::class)
+            ->add('volver_al_listado', ButtonType::class, [
+                'label' => 'Volver al listado',
+                'attr' => [
+                    'onclick' => sprintf("location.href='%s'", $this->urlGenerator->generate('all_proveedor'))
+                ],
+            ]);
         ;
     }
 
