0081adf4a7bef0ac74984047ef4b568dd7af2303
Angela-GM <angelagm0112@gmail.com>
2023-08-27 10:49:27 +0200

    añadir estilos a la tabla del listado de proveedores

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    './src/**/*.twig',
+    './templates/**/*.twig',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
