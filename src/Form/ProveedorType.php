c9be51669edfa3a1a7425b4da9f10a9b52747bf4
Angela-GM <angelagm0112@gmail.com>
2023-08-27 16:00:16 +0200

    crear estilos para el formulario

--- src/Form/ProveedorType.php
+++ src/Form/ProveedorType.php
@@ -36,10 +36,18 @@ class ProveedorType extends AbstractType
                 'class' => Tipo::class,
                 'choice_label' => 'tipo', 
             ])   
-            ->add('submit', SubmitType::class)
-            ->add('volver_al_listado', ButtonType::class, [
-                'label' => 'Volver al listado',
+            ->add('guardar', SubmitType::class, [
+                'label' => '<i class="fa-solid fa-save"></i> Guardar',
+                'label_html' => true,
                 'attr' => [
+                    'class' => 'text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800',
+                ],
+            ])
+            ->add('volver', ButtonType::class, [
+                'label' => '<i class="fa-solid fa-arrow-rotate-left"></i> Volver al listado',
+                'label_html' => true,
+                'attr' => [
+                    'class' => 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
                     'onclick' => sprintf("location.href='%s'", $this->urlGenerator->generate('all_proveedor'))
                 ],
             ]);
